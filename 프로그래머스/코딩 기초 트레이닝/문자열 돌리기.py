54f8da1d91c3299cecaad88cae9841138273145e
yunjuka <lio464628@gmail.com>
2025-05-27 04:28:53 +0900

    기초

---
+++ 프로그래머스/코딩 기초 트레이닝/문자열 돌리기.py
@@ -0,0 +1,16 @@
+
+# str = input()
+# print(str[0:1])
+# print(str[1:2])
+# print(str[2:3])
+# print(str[3:4])
+# print(str[4:5])
+
+# s = input()
+# for i in range(len(s)) :
+#     print(s[i])
+
+
+str = input()
+for a in str:
+    print(a)
---
+++ 프로그래머스/코딩 기초 트레이닝/문자열 붙이기.py
@@ -0,0 +1,5 @@
+
+str1,str2 = input().split()
+print(s1+s2)
+
+# print(input().strip().replace(' ', ''))
